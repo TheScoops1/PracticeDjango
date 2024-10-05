4002203ed889707b7ff66f2fe054a9e2f024526e
TheOnlyScoops1 <scoopingether@gmail.com>
2024-10-05 16:37:11 -0500

    Adding a new model as well as inital views

--- polls/models.py
+++ polls/models.py
@@ -21,3 +21,11 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Anwsers(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    anwser_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.anwser_text
