3afdb22e4ba8a2bc1979226180aadca4a5c7175e
TheOnlyScoops1 <scoopingether@gmail.com>
2024-10-16 09:53:35 -0500

    Fixed the personal website not loading the images correctly as well as editing the admin model pages

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import Question, Choice, Anwser
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+class ChoiceAdmin(admin.ModelAdmin):
+    fields = ['choice_text', 'votes']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Anwser)
